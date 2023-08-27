c1da4230dddb4fd27c23e4dc26b526e3ca7e329c
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-27 14:23:58 -0400

    added functionality to js, updated html

--- livewell_db/js/livewelldb.js
+++ livewell_db/js/livewelldb.js
@@ -11,4 +11,43 @@ fetchMealPlan('app/select.php');
 
 
 
-function displayMealPlan(data) {};
+function displayMealPlan(data) {
+    //select element from HTML to display data
+    const display = document.querySelector('#display');
+    display.innerHTML = '';
+
+    //create a list to display data
+    let ul = document.createElement('ul');
+
+    data.forEach((user) => {
+        // console.log(user);
+        //create items, add text and append to list
+        let li = document.createElement('li');
+        li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
+        ul.appendChild(li);
+    });
+
+    //append list to display element
+display.appendChild(ul);
+}
+
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', getFormData); 
+
+function getFormData(event) {
+    event.preventDefault();
+
+    //get form data & call an async function to post data to API
+    const insertFormData = new FormData(document.querySelector('#insert-form'));
+    let url = 'app/insert.php';
+    inserter(insertFormData, url);
+}
+
+async function inserter(data, url) {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: data
+    });
+    const confirmation = await response.json();
+    console.log(confirmation);
+}
