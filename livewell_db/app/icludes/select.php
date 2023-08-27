b9c8c847f7990e848eabc6890547f97e6259e580
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-27 09:16:16 -0400

    updates

--- livewell_db/app/icludes/select.php
+++ livewell_db/app/icludes/select.php
@@ -1,8 +1,26 @@
 <?php
+
+//include database connection
 require_once 'db_connect.php';
 
+//query all records from the database
+$stmt = $link->prepare($link"SELECT, email, mealplan, timestamp FROM livewell_db ORDER BY timestamp DESC");
 
+//execute query
+mysqli_stmt_execute($stmt);
 
+//get result
+$result = mysqli_stmt_get_result($stmt);
 
+//loop through the returned data
+while ($row = mysqli_fetch_assoc($result)) {
+   $results[] = $row;
+}
+
+//return json encoded array
+echo json_encode($results);
+
+//close connection
+mysqli_close($link);
 
 ?>
