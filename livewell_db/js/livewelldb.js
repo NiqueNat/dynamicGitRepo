53b68d8b73046a35083bea102a5a86262813d038
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-27 14:58:03 -0400

    Update livewelldb.js

--- livewell_db/js/livewelldb.js
+++ livewell_db/js/livewelldb.js
@@ -3,7 +3,7 @@
 async function fetchMealPlan(url) {
     const response = await fetch(url);
     const data = await response.json();
-    // displayMealPlan(data);
+    displayMealPlan(data);
     console.log(data);
 }
 
