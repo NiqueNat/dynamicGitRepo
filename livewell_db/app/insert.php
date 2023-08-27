55f46c7eff65fbd5d983c71adf263cf876ed9389
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-27 14:55:30 -0400

    trying to fix errors

--- livewell_db/app/insert.php
+++ livewell_db/app/insert.php
@@ -1,64 +1,54 @@
 <?php
 require_once "./includes/db_includes.php";
 
-//used to store the results of the query
+// Used to store the results of the query
 $results = [];
 $insertedRows = 0;
 
-//sql query copied from phpMyAdmin
-//INSERT INTO `livewell_db` (`livewellID`, `name`, `email`, `meal plan`, `timestamp`) VALUES (NULL, 'Shannon Curry', 'scurry@gmail.com', 'Keto', current_timestamp());
-
 try {
-   if(!isset($_REQUEST['full_name']) || !isset($_REQUEST['email']) || !isset($_REQUEST['meal_plan']) ){
-      throw new Exception('Required data missing i.e full-name, email, meal_plan');
+    if (!isset($_REQUEST['full_name']) || !isset($_REQUEST['email']) || !isset($_REQUEST['meal_plan'])) {
+        throw new Exception('Required data missing i.e full-name, email, meal_plan');
     }
 
-//insert the data into the database
-$query = "INSERT INTO livewell_db (name, email, `meal plan`) VALUES (?, ?, ?)";
+    // Insert the data into the database
+    $query = "INSERT INTO livewell_db (name, email, `meal plan`) VALUES (?, ?, ?)";
 
-//prepare the query
-if ($stmt = mysqli_prepare($link, $query)) {
-    mysqli_stmt_bind_param($stmt, 'sss', $_REQUEST["full_name"], $_REQUEST['email'], $_REQUEST['meal_plan']);
-    mysqli_stmt_execute($stmt);
-    $insertedRows = mysqli_stmt_affected_rows($stmt);
+    // Prepare the query
+    if ($stmt = mysqli_prepare($link, $query)) {
+        mysqli_stmt_bind_param($stmt, 'sss', $_REQUEST["full_name"], $_REQUEST['email'], $_REQUEST['meal_plan']);
+        mysqli_stmt_execute($stmt);
+        $insertedRows = mysqli_stmt_affected_rows($stmt);
 
-    //if the query was successful, return the data
-    if ($insertedRows > 0) {
-        $results[] = [
+        // If the query was successful, prepare the response data
+        $response = [
+            "success" => true,
+            "message" => "Data inserted successfully",
             "insertedRows" => $insertedRows,
             "id" => mysqli_insert_id($link),
-            "full_name" => $_REQUEST["full_name"]
+            "full_name" => $_REQUEST["full_name"],
+            "email" => $_REQUEST["email"],
+            "meal_plan" => $_REQUEST["meal_plan"]
         ];
-    }else{
-        throw new Exception('No rows were inserted');
+
+        mysqli_stmt_close($stmt);
+    } else {
+        throw new Exception('Prepared statement did not insert records');
     }
-
-    
-  
-    mysqli_stmt_close($stmt);
-
-
-}else{
-    throw new Exception('Prepared statement did not insert records');
+} catch (Exception $error) {
+    $response = [
+        "success" => false,
+        "error" => $error->getMessage()
+    ];
 }
 
-
-
-
-   } catch($Exception $error){
-        echo json_encode(['error' => $error->getMessage()]);
-    } finally{
-            echo json_encode([
-                "message" => $_REQUEST["full_name"],
-                "email" => $_REQUEST["email"],
-                "meal_plan" => $_REQUEST["meal_plan"]
-            ]);
-        }
-
-
-close the connection
+// Close the connection
 mysqli_close($link);
+
+// Echo the response as JSON
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
+
 
 
 <!-- https://myrna67.web582.com/livewell_db/app/insert.php?full_name=Honey&email=hsmtih@gmail.com&meal%20plan=vegan -->
--- livewell_db/js/livewelldb.js
+++ livewell_db/js/livewelldb.js
@@ -3,52 +3,53 @@
 async function fetchMealPlan(url) {
     const response = await fetch(url);
     const data = await response.json();
-    displayMealPlan(data);
+    // displayMealPlan(data);
+    console.log(data);
 }
 
 //call fetchMealPlan() with the url of the API
-fetchMealPlan('app/select.php');
+fetchMealPlan('./app/select.php');
 
 
 
-function displayMealPlan(data) {
-    //select element from HTML to display data
-    const display = document.querySelector('#display');
-    display.innerHTML = '';
+// function displayMealPlan(data) {
+//     //select element from HTML to display data
+//     const display = document.querySelector('#display');
+//     display.innerHTML = '';
 
-    //create a list to display data
-    let ul = document.createElement('ul');
+//     //create a list to display data
+//     let ul = document.createElement('ul');
 
-    data.forEach((user) => {
-        // console.log(user);
-        //create items, add text and append to list
-        let li = document.createElement('li');
-        li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
-        ul.appendChild(li);
-    });
+//     data.forEach((user) => {
+//         // console.log(user);
+//         //create items, add text and append to list
+//         let li = document.createElement('li');
+//         li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
+//         ul.appendChild(li);
+//     });
 
-    //append list to display element
-display.appendChild(ul);
-}
+//     //append list to display element
+// display.appendChild(ul);
+// }
 
-const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', getFormData); 
+// const submitButton = document.querySelector('#submit');
+// submitButton.addEventListener('click', getFormData); 
 
-function getFormData(event) {
-    event.preventDefault();
+// function getFormData(event) {
+//     event.preventDefault();
 
-    //get form data & call an async function to post data to API
-    const insertFormData = new FormData(document.querySelector('#insert-form'));
-    let url = 'app/insert.php';
-    inserter(insertFormData, url);
-}
+//     //get form data & call an async function to post data to API
+//     const insertFormData = new FormData(document.querySelector('#insert-form'));
+//     let url = 'app/insert.php';
+//     inserter(insertFormData, url);
+// }
 
-async function inserter(data, url) {
-    const response = await fetch(url, {
-        method: 'POST',
-        body: data
-    });
-    const confirmation = await response.json();
-    fetchMealPlan('app/select.php');
-    console.log(confirmation);
-}
+// async function inserter(data, url) {
+//     const response = await fetch(url, {
+//         method: 'POST',
+//         body: data
+//     });
+//     const confirmation = await response.json();
+//     fetchMealPlan('app/select.php');
+//     console.log(confirmation);
+// }
