e0d7a5b29bc37b6b1801a1ac9dab3a5aac6db70a
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-27 10:27:59 -0400

    Update insert.php

--- livewell_db/app/insert.php
+++ livewell_db/app/insert.php
@@ -5,5 +5,26 @@ $results = [];
 $insertedRows = 0;
 
 
+//INSERT INTO `livewell_db` (`livewellID`, `name`, `email`, `meal plan`, `timestamp`) VALUES (NULL, 'Shannon Curry', 'scurry@gmail.com', 'Keto', current_timestamp());
 
+$query = "INSERT INTO livewell_db (name, email, meal plan) VALUES (?, ?, ?);"
+
+if($stmt = mysqli_prepare($link, $query)) {
+    mysqli_stmt_bind_param($stmt, "sss",$_REQUEST["full_name"], $_REQUEST['email'], $_REQUEST['meal plan']);
+    mysqli_stmt_execute($stmt);
+    $insertedRows = mysqli_stmt_affected_rows($stmt);
+    mysqli_stmt_close($stmt);
+
+    if($insertedRows > 0) {
+        $results[] = [
+            "insertedRows" => $insertedRows,
+            "id" => $link->insert_id,
+            "full_name" => $_REQUEST["full_name"],
+        ];
+
+}
+echo json_encode($results);
+}
+
+//https://myrna67.web582.com/livewell_db/app/insert.php?full_name=Honey&email=hsmtih@gmail.com&meal%20plan=vegan
 ?>
