4df9652ecc66c9a5fb77367d87a9fb7f6a8e326f
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-28 16:55:36 -0400

    updates

--- livewell_db/js/livewelldb.js
+++ livewell_db/js/livewelldb.js
@@ -1,49 +1,3 @@
-async function fetchMealPlan(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    displayMealPlan(data);
-    updateChart(data); // Call the function to update the chart
-}
-
-// Call fetchMealPlan() with the url of the API
-fetchMealPlan('app/select.php');
-
-function displayMealPlan(data) {
-    const display = document.querySelector('#display');
-    display.innerHTML = '';
-
-    let ul = document.createElement('ul');
-
-    data.forEach((user) => {
-        let li = document.createElement('li');
-        li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
-        ul.appendChild(li);
-    });
-
-    display.appendChild(ul);
-}
-
-const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', getFormData);
-
-async function getFormData(event) {
-    event.preventDefault();
-
-    const insertFormData = new FormData(document.querySelector('#insert-form'));
-    let url = 'app/insert.php';
-    await inserter(insertFormData, url); // Wait for inserter to complete
-    fetchMealPlan('app/select.php');
-}
-
-async function inserter(data, url) {
-    const response = await fetch(url, {
-        method: 'POST',
-        body: data
-    });
-    const confirmation = await response.text();
-    console.log(confirmation);
-}
-
 // This function updates the chart
 function updateChart(data) {
     const mealPlanCounts = {}; // Count occurrences of each meal plan
@@ -60,7 +14,7 @@ function updateChart(data) {
     const counts = Object.values(mealPlanCounts);
 
     // Update the chart using Chart.js
-    const ctx = document.getElementById('chartCanvas').getContext('2d');
+    const ctx = document.getElementById('myChart').getContext('2d'); // Use 'myChart' instead of 'chartCanvas'
     new Chart(ctx, {
         type: 'bar',
         data: {
@@ -77,5 +31,6 @@ function updateChart(data) {
         }
     });
 }
+
 
 
