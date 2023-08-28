311eea25f61719b89f2747befca0fc926f225759
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-28 15:37:47 -0400

    testing

--- livewell_db/chart.js
+++ livewell_db/chart.js
@@ -1,22 +1,22 @@
 // Remove the import statement
- import Chart from 'chart.js/auto';
+//  import Chart from 'chart.js/auto';
 
-// Keep the rest of the code as is
-const ctx = document.getElementById('chartCanvas').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Vegan', 'Vegetarian', 'Keto'],
-        datasets: [{
-            label: 'Meal Plans',
-            data: [20, 15, 10],
-            backgroundColor: ['red', 'green', 'blue']
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false
-    }
-});
+// // Keep the rest of the code as is
+// const ctx = document.getElementById('chartCanvas').getContext('2d');
+// const myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Vegan', 'Vegetarian', 'Keto'],
+//         datasets: [{
+//             label: 'Meal Plans',
+//             data: [20, 15, 10],
+//             backgroundColor: ['red', 'green', 'blue']
+//         }]
+//     },
+//     options: {
+//         responsive: true,
+//         maintainAspectRatio: false
+//     }
+// });
 
 
