7211b9937e5b52bea3a73f48afd12b2e1e21ce9f
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-28 16:25:03 -0400

    updated file to include chart.js

--- livewell_db/js/livewelldb.js
+++ livewell_db/js/livewelldb.js
@@ -1,55 +1,90 @@
+// Fetch the chart canvas element
+const chartCanvas = document.getElementById('myChart');
+
+// Initialize the chart data with zeros
+const initialChartData = {
+  labels: ['Vegan', 'Vegetarian', 'Keto'],
+  datasets: [
+    {
+      label: 'Meal Plans',
+      data: [0, 0, 0],
+      backgroundColor: ['red', 'green', 'blue'],
+    },
+  ],
+};
+
+// Create a Chart instance with initial data
+const myChart = new Chart(chartCanvas, {
+  type: 'bar',
+  data: initialChartData,
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+  },
+});
+
+// Function to fetch and display meal plans
 async function fetchMealPlan(url) {
+  try {
     const response = await fetch(url);
     const data = await response.json();
     displayMealPlan(data);
+  } catch (error) {
+    console.error('Error fetching meal plans:', error);
+  }
 }
 
-//call fetchMealPlan() with the url of the API
+// Call fetchMealPlan() with the url of the API
 fetchMealPlan('app/select.php');
 
-
-
+// Display meal plans in the HTML
 function displayMealPlan(data) {
-    //select element from HTML to display data
-    const display = document.querySelector('#display');
-    display.innerHTML = '';
+  const display = document.querySelector('#display');
+  display.innerHTML = '';
 
-    //create a list to display data
-    let ul = document.createElement('ul');
+  let ul = document.createElement('ul');
 
-    data.forEach((user) => {
-        // console.log(user);
-        //create items, add text and append to list
-        let li = document.createElement('li');
-        li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
-        ul.appendChild(li);
-    });
+  data.forEach((user) => {
+    let li = document.createElement('li');
+    li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
+    ul.appendChild(li);
+  });
 
-    //append list to display element
-display.appendChild(ul);
+  display.appendChild(ul);
 }
 
-const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', getFormData); 
+// Form submission handler
+async function getFormData(event) {
+  event.preventDefault();
 
-function getFormData(event) {
-    event.preventDefault();
+  const insertFormData = new FormData(document.querySelector('#insert-form'));
+  const url = 'app/insert.php';
 
-    //get form data & call an async function to post data to API
-    const insertFormData = new FormData(document.querySelector('#insert-form'));
-    let url = 'app/insert.php';
-    inserter(insertFormData, url);
-}
-
-async function inserter(data, url) {
+  try {
     const response = await fetch(url, {
-        method: 'POST',
-        body: data
+      method: 'POST',
+      body: insertFormData,
     });
-    //using await to wait for the response for the server
-    const confirmation = await response.text();
-    fetchMealPlan('app/select.php');
-    console.log(confirmation);
+    const data = await response.json();
+
+    // Update chart data based on the meal plan received
+    if (data.meal_plan === 'Vegan') {
+      myChart.data.datasets[0].data[0] += 1;
+    } else if (data.meal_plan === 'Vegetarian') {
+      myChart.data.datasets[0].data[1] += 1;
+    } else if (data.meal_plan === 'Keto') {
+      myChart.data.datasets[0].data[2] += 1;
+    }
+
+    // Update the chart with the new data
+    myChart.update();
+  } catch (error) {
+    console.error('Error submitting form:', error);
+  }
 }
+
+// Attach the form submission event listener
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', getFormData);
 
 
