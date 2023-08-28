ec871350b34a887c80f84e303b47217716469b51
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-28 16:35:57 -0400

    testing

--- livewell_db/js/livewelldb.js
+++ livewell_db/js/livewelldb.js
@@ -1,90 +1,81 @@
-// Fetch the chart canvas element
-const chartCanvas = document.getElementById('myChart');
-
-// Initialize the chart data with zeros
-const initialChartData = {
-  labels: ['Vegan', 'Vegetarian', 'Keto'],
-  datasets: [
-    {
-      label: 'Meal Plans',
-      data: [0, 0, 0],
-      backgroundColor: ['red', 'green', 'blue'],
-    },
-  ],
-};
-
-// Create a Chart instance with initial data
-const myChart = new Chart(chartCanvas, {
-  type: 'bar',
-  data: initialChartData,
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-  },
-});
-
-// Function to fetch and display meal plans
 async function fetchMealPlan(url) {
-  try {
     const response = await fetch(url);
     const data = await response.json();
     displayMealPlan(data);
-  } catch (error) {
-    console.error('Error fetching meal plans:', error);
-  }
+    updateChart(data); // Call the function to update the chart
 }
 
 // Call fetchMealPlan() with the url of the API
 fetchMealPlan('app/select.php');
 
-// Display meal plans in the HTML
 function displayMealPlan(data) {
-  const display = document.querySelector('#display');
-  display.innerHTML = '';
+    const display = document.querySelector('#display');
+    display.innerHTML = '';
 
-  let ul = document.createElement('ul');
+    let ul = document.createElement('ul');
 
-  data.forEach((user) => {
-    let li = document.createElement('li');
-    li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
-    ul.appendChild(li);
-  });
-
-  display.appendChild(ul);
-}
-
-// Form submission handler
-async function getFormData(event) {
-  event.preventDefault();
-
-  const insertFormData = new FormData(document.querySelector('#insert-form'));
-  const url = 'app/insert.php';
-
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      body: insertFormData,
+    data.forEach((user) => {
+        let li = document.createElement('li');
+        li.innerHTML = `${user.meal_plan_id} ${user.meal_plan_name}.`;
+        ul.appendChild(li);
     });
-    const data = await response.json();
 
-    // Update chart data based on the meal plan received
-    if (data.meal_plan === 'Vegan') {
-      myChart.data.datasets[0].data[0] += 1;
-    } else if (data.meal_plan === 'Vegetarian') {
-      myChart.data.datasets[0].data[1] += 1;
-    } else if (data.meal_plan === 'Keto') {
-      myChart.data.datasets[0].data[2] += 1;
-    }
-
-    // Update the chart with the new data
-    myChart.update();
-  } catch (error) {
-    console.error('Error submitting form:', error);
-  }
+    display.appendChild(ul);
 }
 
-// Attach the form submission event listener
 const submitButton = document.querySelector('#submit');
 submitButton.addEventListener('click', getFormData);
+
+async function getFormData(event) {
+    event.preventDefault();
+
+    const insertFormData = new FormData(document.querySelector('#insert-form'));
+    let url = 'app/insert.php';
+    await inserter(insertFormData, url); // Wait for inserter to complete
+    fetchMealPlan('app/select.php');
+}
+
+async function inserter(data, url) {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: data
+    });
+    const confirmation = await response.text();
+    console.log(confirmation);
+}
+
+// This function updates the chart
+function updateChart(data) {
+    const mealPlanCounts = {}; // Count occurrences of each meal plan
+
+    data.forEach((user) => {
+        if (mealPlanCounts[user.meal_plan_name]) {
+            mealPlanCounts[user.meal_plan_name]++;
+        } else {
+            mealPlanCounts[user.meal_plan_name] = 1;
+        }
+    });
+
+    const labels = Object.keys(mealPlanCounts);
+    const counts = Object.values(mealPlanCounts);
+
+    // Update the chart using Chart.js
+    const ctx = document.getElementById('chartCanvas').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Meal Plan Counts',
+                data: counts,
+                backgroundColor: 'blue'
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+}
 
 
