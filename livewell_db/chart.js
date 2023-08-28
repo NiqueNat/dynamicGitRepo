66b3580f2bef5f4cc43a6330e3671d0d0fa67b0f
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-28 16:02:18 -0400

    updates

--- livewell_db/chart.js
+++ livewell_db/chart.js
@@ -1,22 +1,35 @@
 // Remove the import statement
- import Chart from 'chart.js/auto';
+//  import Chart from 'chart.js/auto';
+//  const myChart = new Chart("myChart", {
+//     type: "bar",
+//     data: {},
+//     options: {}
+//   });
 
-// Keep the rest of the code as is
-const ctx = document.getElementById('chartCanvas').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Vegan', 'Vegetarian', 'Keto'],
-        datasets: [{
-            label: 'Meal Plans',
-            data: [20, 15, 10],
-            backgroundColor: ['red', 'green', 'blue']
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false
-    }
+const xValues = [100,200,300,400,500,600,700,800,900,1000];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{ 
+      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+      borderColor: "red",
+      fill: false
+    }, { 
+      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+      borderColor: "green",
+      fill: false
+    }, { 
+      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+      borderColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+    legend: {display: false}
+  }
 });
+
 
 
