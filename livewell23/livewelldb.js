826bb2d07a1746886cef471199656cfcb7ca5f78
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-09-11 13:49:33 -0400

    chart and activity counter update

--- livewell23/livewelldb.js
+++ livewell23/livewelldb.js
@@ -34,7 +34,7 @@ async function getFormData() {
 
   try {
     const response = await fetch('./insert.php', {
-      method: 'POST', // Change the request method to POST
+      method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
@@ -44,27 +44,20 @@ async function getFormData() {
     console.log('Response Status:', response.status); // Log the response status
     console.log('Response Headers:', response.headers); // Log the response headers
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+    if (!response.ok) throw new Error('Network response was not ok');
 
     const contentType = response.headers.get('content-type');
     console.log('Content-Type:', contentType); // Log the content type
 
+    const responseBody = await response.text(); // Read the response body as text
+    console.log('Response Body:', responseBody); // Log the response body
+
+    let responseData; // Variable to store the JSON response data
+
     if (contentType && contentType.indexOf('application/json') !== -1) {
-      const responseData = await response.json();
+      responseData = JSON.parse(responseBody); // Parse JSON response data
       console.log('JSON Data:', responseData); // Log the JSON data
-
-      // Check if the response indicates success
-      if (responseData.success) {
-        // Data was inserted successfully, you can perform any necessary actions here.
-        console.log(responseData.message);
-
-        // Update the chart with meal plan data
-        updateChart(responseData.mealPlanData);
-      } else {
-        console.error('Error:', responseData.message);
-      }
+      updateChart(responseData);
     } else {
       throw new Error('Oops, we haven\'t got JSON!');
     }
@@ -73,17 +66,93 @@ async function getFormData() {
   }
 }
 
-function updateChart(mealPlanData) {
-  if (mealPlanData) {
-    const labels = mealPlanData.map((item) => item.meal_plan);
-    const counts = mealPlanData.map((item) => item.count);
+function updateChart(data) {
+  if (data?.success && data?.mealPlanData) {
+    const labels = data.mealPlanData.map((item) => item.meal_plan);
+    const counts = data.mealPlanData.map((item) => item.count);
 
     myChart.data.labels = labels;
     myChart.data.datasets[0].data = counts;
     myChart.update();
+  } else if (!data?.success) {
+    console.error('Unsuccessful response received: ', data?.error || 'No error message provided');
   } else {
-    console.error('No mealPlanData provided in the response');
+    console.error('Unexpected response data format: ', data || 'No data provided');
   }
 }
 
-// Rest of your code...
+// Activity Calorie Counter Code
+
+const openFormBtn = document.getElementById('openFormBtn');
+const formModal = document.getElementById('formModal');
+const closeFormBtn = document.getElementById('closeFormBtn');
+
+const resultModal = document.getElementById('resultModal');
+const closeResultBtn = document.getElementById('closeResultBtn');
+
+// Open the form modal
+openFormBtn.onclick = function () {
+  formModal.style.display = 'block';
+};
+
+// Close the form modal
+closeFormBtn.onclick = function () {
+  formModal.style.display = 'none';
+};
+
+// Close the results modal
+closeResultBtn.onclick = function () {
+  resultModal.style.display = 'none';
+};
+
+const form = document.getElementById('exercise-form');
+const resultsList = document.getElementById('results-list');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  try {
+    const weight = document.getElementById('weight-input').value;
+    const duration = document.getElementById('duration-input').value;
+    const activity = document.getElementById('activity-input').value;
+
+    const url = `https://api.api-ninjas.com/v1/caloriesburned?activity=${activity}&weight=${weight}&duration=${duration}`;
+
+    const response = await fetch(url, {
+      headers: {
+        'X-Api-Key': 'OiPSiafhj0bRTltU0NE9uA==7NujYvqQNPjCP14V', 
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Error fetching calories burned data');
+    }
+
+    const data = await response.json();
+
+    if (Array.isArray(data) && data.length > 0) {
+      resultsList.innerHTML = '';
+      data.forEach((entry) => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+          <p>Activity: ${entry.name}</p>
+          <p>Calories Per Hour: ${entry.calories_per_hour}</p>
+          <p>Duration: ${entry.duration_minutes} minutes</p>
+          <p>Calories Burned: ${entry.total_calories ? entry.total_calories.toFixed(2) : 'N/A'}</p>
+        `;
+        resultsList.appendChild(listItem);
+      });
+      formModal.style.display = 'none'; // Hide the form modal
+      resultModal.style.display = 'block'; // Show the result modal
+    } else {
+      resultsList.innerHTML = 'Calories Burned data not available.';
+      formModal.style.display = 'none'; // Hide the form modal
+      resultModal.style.display = 'block'; // Show the result modal
+    }
+  } catch (error) {
+    console.error('Error fetching calories burned data:', error);
+    resultsList.innerHTML = 'Error fetching data. Please try again.';
+    formModal.style.display = 'none'; // Hide the form modal
+    resultModal.style.display = 'block'; // Show the result modal
+  }
+});
